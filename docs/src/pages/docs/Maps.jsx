b8246a0a9bd5b899f0f04dcc10ae89d76546e354
Charles Graham <charles.r.graham@usace.army.mil>
2024-07-25 16:52:53 -0500

    Fix display of code import for endusers

--- docs/src/pages/docs/Maps.jsx
+++ docs/src/pages/docs/Maps.jsx
@@ -10,8 +10,7 @@ export default function Maps() {
     </div>
     <H3>Code Example:</H3>
     <Code className="mt-8" syntaxHighlight={true} language="jsx">
-      {`import { GageMap } from "@usace-watermanagement/groundwork-water";
-import { Code } from "../../../../lib/components/display/text";
+      {`import { GageMap, Code} from "@usace-watermanagement/groundwork-water";
 
 export default function Maps() {
   return (
