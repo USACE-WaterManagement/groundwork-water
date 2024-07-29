6122d84eb1b056c1d2fc5d9824ee864e5005c6b0
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-07-29 13:45:54 -0400

    Add useCdaLocation hook

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,9 +1,10 @@
 // Import components
-import TSPlot from './components/data/plots/TSPlot';
-import TSTable from './components/data/tables/TSTable';
-import GageMap from "./components/data/maps/GageMap"
+import TSPlot from "./components/data/plots/TSPlot";
+import TSTable from "./components/data/tables/TSTable";
+import GageMap from "./components/data/maps/GageMap";
 import CWMSPlot from "./components/data/plots/CWMSPlot";
+import useCdaLocation from "./components/data/hooks/useCdaLocation";
 // import { helperFunction } from './utils/helpers';
 
-export { TSPlot, TSTable, GageMap, CWMSPlot};
+export { TSPlot, TSTable, GageMap, CWMSPlot, useCdaLocation };
 // export { helperFunction };
