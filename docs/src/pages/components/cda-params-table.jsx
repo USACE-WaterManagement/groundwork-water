cb06b4177e90f3e2f91177d2c3cd67074d4030d7
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-07-30 09:01:31 -0400

    Import docs helper components from groundwork
    Made a few minor modifications to be consistent with groundwork-water

---
+++ docs/src/pages/components/cda-params-table.jsx
@@ -0,0 +1,32 @@
+import ParamsTable from "./params-table";
+import { cdaUrlParam, queryOptionsParam } from "./shared-docs";
+
+const CdaParamsTable = ({ requestObject, requestType, cwmsJsType }) => {
+  const docsBase =
+    "https://hydrologicengineeringcenter.github.io/cwms-data-api-client-javascript/interfaces/";
+  const docsUrl = `${docsBase}${cwmsJsType}.html`;
+  const hookParams = [
+    {
+      name: "cdaParams",
+      type: "object",
+      required: true,
+      desc: (
+        <>
+          Parameters provided to CDA for a {requestObject} {requestType}{" "}
+          request. Core parameters are listed below. Check the{" "}
+          <a href={docsUrl} className="underline">
+            cwmsjs documentation
+          </a>{" "}
+          for a full listing.
+        </>
+      ),
+    },
+    cdaUrlParam,
+    queryOptionsParam,
+  ];
+
+  return <ParamsTable paramsList={hookParams} />;
+};
+
+export { CdaParamsTable };
+export default CdaParamsTable;
