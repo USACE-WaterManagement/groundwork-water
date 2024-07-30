be909b123c4471a1bb9cac1177534e8ed8684ee5
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-07-30 10:22:37 -0400

    Add useCdaCatalog hook

--- lib/index.jsx
+++ lib/index.jsx
@@ -3,8 +3,9 @@ import TSPlot from "./components/data/plots/TSPlot";
 import TSTable from "./components/data/tables/TSTable";
 import GageMap from "./components/data/maps/GageMap";
 import CWMSPlot from "./components/data/plots/CWMSPlot";
+import useCdaCatalog from "./components/data/hooks/useCdaCatalog";
 import useCdaLocation from "./components/data/hooks/useCdaLocation";
 // import { helperFunction } from './utils/helpers';
 
-export { TSPlot, TSTable, GageMap, CWMSPlot, useCdaLocation };
+export { TSPlot, TSTable, GageMap, CWMSPlot, useCdaCatalog, useCdaLocation };
 // export { helperFunction };
