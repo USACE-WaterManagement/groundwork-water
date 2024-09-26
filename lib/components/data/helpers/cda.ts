6156754a42a7484d9ec5bba2fe985a4c611a55fc
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-09-26 15:12:40 -0400

    Convert cda helpers to TypeScript

--- lib/components/data/helpers/cda.ts
+++ lib/components/data/helpers/cda.ts
@@ -1,8 +1,12 @@
+import { TimeSeries } from "cwmsjs";
+
 /**
  * Retrieve the chronologically-last entry returned from a CDA TimeSeries GET request.
  * @param {object} cdaTimeSeries A valid CDA TimeSeries GET response.
  * @returns {[number, number, number]} The last CDA TimeSeries entry.
  */
-export const getLatestEntry = (cdaTimeSeries) => {
-  return cdaTimeSeries.values.filter((entry) => entry[1] !== null).slice(-1)[0];
+export const getLatestEntry = (cdaTimeSeries: TimeSeries) => {
+  return cdaTimeSeries.values
+    ?.filter((entry) => entry[1] !== null)
+    .slice(-1)[0];
 };
