755aab7d96ad7c4b7ab1a8a6bd248c7461cef388
stephenkissock <stephenkissock@gmail.com>
2024-10-16 16:45:37 -0400

    Updates to CWMSPlot and starting a new CWMSTable

--- lib/components/data/plots/CWMSPlot.jsx
+++ lib/components/data/plots/CWMSPlot.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Configuration, TimeSeriesApi } from "cwmsjs";
 import Plotly from "plotly.js-basic-dist";
-import dayjs from "dayjs";
 import { gwMerge } from "@usace/groundwork";
 
 const config_v2 = new Configuration({
@@ -18,26 +17,28 @@ export default function CWMSPlot({
   begin,
   end,
   title,
-  fontSize,
   unit = "EN",
+  timeseriesParams,
+  layoutParams,
   className = "",
-  plotHeight = 550,
-  shapes = [],
-  autoSize = true,
-  annotations = [],
   responsive = true,
   loadingComponent = null,
-  layoutGrid = null,
 }) {
   const plotElement = useRef(null);
-  const [metaData, setMetaData] = useState(null);
   const [plotTitle, setPlotTitle] = useState(null);
   const [plotTSIDs, setPlotTSIDs] = useState(null);
-  const [plotFontSize, setPlotFontSize] = useState(fontSize);
+
+
+  const layout = layoutParams
+
+
+  const pageSize = 500
+
+
 
   useEffect(() => {
     if (!title) {
-      setPlotTitle((Array.isArray(tsids) ? tsids[0] : tsids).split(".")[0]);
+      setPlotTitle(layout.title.text);
     }
     if (!tsids.length)
       throw Error("You must specify one or more Timeseries IDs to plot.");
@@ -53,9 +54,10 @@ export default function CWMSPlot({
       try {
         return await ts_api.getCwmsDataTimeseries({
           name,
-          end,
-          unit,
           begin,
+          end,
+          //pageSize,
+          unit,
           office,
         });
       } catch (error) {
@@ -90,7 +92,8 @@ export default function CWMSPlot({
     error,
     isLoading,
   } = useQuery({
-    queryKey: ["timeseries", plotTSIDs, begin, end, unit, office],
+    queryKey: ["timeseries", plotTSIDs, begin, end, unit, //pageSize, 
+      office],
     queryFn: fetchData,
     enabled: !!plotElement.current, // Only run the query when plotElement is available
   });
@@ -100,79 +103,37 @@ export default function CWMSPlot({
     }
 
     let unit_keys = Object.keys(tsData.ts);
-    let grid_col_cnt = unit_keys.length;
-    const layout = {
-      autosize: autoSize,
-      title: title,
-      shapes: shapes,
-      annotations: annotations,
-    };
-    if (title) layout.title = title;
-    else
-      layout.title = {
-        text: `${plotTitle}<br>Units: ${unit_keys.join(", ")}`,
-        font: {
-          family: "DejaVuSansMono, monospace",
-        },
-      };
-    if (layoutGrid) layout.grid = layoutGrid;
-    else
-      layout.grid = { rows: grid_col_cnt, columns: 1, pattern: "independent" };
-
-    let traces = [];
-    let trace_cnt = 1;
-    let domain_start = 0;
-    let domain_delta = 1 / unit_keys.length;
-    let domain_end = domain_delta;
-
-    // Force the font size if specified
-    if (plotFontSize) layout["font"] = { size: plotFontSize };
-    else if (unit_keys.length > 4) {
-      layout["font"] = { size: 8 };
-    }
 
     // Create traces keyed to the unit
+    let traces = [];
+    let trace_cnt = 1;
     let ts;
     for (let k_idx = 0; k_idx < unit_keys.length; k_idx++) {
       const key = unit_keys[k_idx];
       for (let ts_idx = 0; ts_idx < tsData.ts[key].length; ts_idx++) {
         ts = tsData.ts[key][ts_idx];
+        const params = timeseriesParams.filter((item) => item.tsid == ts.name)
+
         const trace = {
           x: ts.values.map((value) => new Date(value[0])),
           y: ts.values.map((value) => value[1]),
-          yaxis: "y" + trace_cnt,
-          type: "scatter",
-          mode: "lines",
+          name: params[0].name,
+          mode: params[0].mode,
+          type: params[0].type,
+          marker: params[0].marker,
+          line: params[0].line,
           showlegend: true,
           legend: { x: 1, xanchor: "right", y: 1 },
-          name: `${ts.name.split(".")[1]}<br>${ts.name.split(".")[5]}`,
+          yaxis: params[0].yaxis,
         };
         traces.push(trace);
-      }
-      let title_text = `${ts.name.split(".")[1]}<br>(${key})`;
-      if (unit_keys.length > 3) title_text = key;
 
-      layout["yaxis" + trace_cnt] = {
-        domain: [
-          Math.round(domain_start * 100) / 100,
-          Math.round(domain_end * 100) / 100,
-        ],
-        title: { text: title_text },
-      };
-      // Alter if there are more than 4 units / view ports
-      if (unit_keys.length > 4) {
-        layout["yaxis" + trace_cnt]["nticks"] = 1;
-        layout["yaxis" + trace_cnt]["tickvals"] = [
-          ts.values[ts.values.length / 2],
-        ];
-        layout["yaxis" + trace_cnt]["dtick"] = 1;
-        layout["yaxis" + trace_cnt]["tick0"] = 0.5;
       }
 
-      domain_start += domain_delta;
-      domain_end += domain_delta;
+
       trace_cnt++;
     }
+
 
     Plotly.newPlot(plotElement.current, traces, layout, {
       responsive: responsive,
@@ -182,13 +143,13 @@ export default function CWMSPlot({
   return (
     <div
       className={gwMerge("gww-h-full gww-w-full", className)}
-      style={{ height: plotHeight }}
+      style={{ height: layoutParams.height }}
     >
       <div
         ref={plotElement}
         id="plot"
         className="gww-h-full gww-w-full"
-        style={{ height: plotHeight }}
+        style={{ height: layoutParams.height }}
       >
         {isLoading ? (
           loadingComponent ? (
