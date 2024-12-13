9bf43c887b25b9fdb52c6be9b39da39922734ab9
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2024-12-13 13:15:02 -0600

    Format vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,48 +5,54 @@ import pkg from "./package.json";
 
 // Based off of Groundwork proper's lib vs doc config
 export default defineConfig(({ mode }) => {
-    if (mode === "lib") {
-        console.log("Building library");
-        return {
-            plugins: [react(), tailwindcss()],
-            publicDir: false,
-            build: {
-                minify: false,
-                lib: {
-                    name: "GroundworkWater",
-                    fileName: (format) => `groundwork-water.${format}.js`,
-                    entry: "lib/index.jsx",
-                    formats: ['es', 'umd']
-                },
-                rollupOptions: {
-                    external: ["react", "react-dom", "@tanstack/react-query", "ol", "plotly.js-basic-dist"],
-                    output: {
-                        globals: {
-                            react: "React",
-                            "react-dom": "ReactDOM",
-                            "@tanstack/react-query": "ReactQuery",
-                            ol: "ol",
-                            "plotly.js-basic-dist": "Plotly",
-                        },
-                    },
-                },
+  if (mode === "lib") {
+    console.log("Building library");
+    return {
+      plugins: [react(), tailwindcss()],
+      publicDir: false,
+      build: {
+        minify: false,
+        lib: {
+          name: "GroundworkWater",
+          fileName: (format) => `groundwork-water.${format}.js`,
+          entry: "lib/index.jsx",
+          formats: ["es", "umd"],
+        },
+        rollupOptions: {
+          external: [
+            "react",
+            "react-dom",
+            "@tanstack/react-query",
+            "ol",
+            "plotly.js-basic-dist",
+          ],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+              "@tanstack/react-query": "ReactQuery",
+              ol: "ol",
+              "plotly.js-basic-dist": "Plotly",
             },
-        }
-    } else {
-        console.log("Building Docs App: ", mode);
-        const base =
-            mode === "production"
-                ? "https://USACE-WaterManagement.github.io/groundwork-water/"
-                : "http://localhost:5173/";
-        return {
-            plugins: [react(), tailwindcss()],
-            base: base,
-            build: {
-                outDir: "docs",
-            },
-            define: {
-                "import.meta.env.PKG_VERSION": JSON.stringify(pkg.version),
-            },
-        };
-    }
-})
+          },
+        },
+      },
+    };
+  } else {
+    console.log("Building Docs App: ", mode);
+    const base =
+      mode === "production"
+        ? "https://USACE-WaterManagement.github.io/groundwork-water/"
+        : "http://localhost:5173/";
+    return {
+      plugins: [react(), tailwindcss()],
+      base: base,
+      build: {
+        outDir: "docs",
+      },
+      define: {
+        "import.meta.env.PKG_VERSION": JSON.stringify(pkg.version),
+      },
+    };
+  }
+});
