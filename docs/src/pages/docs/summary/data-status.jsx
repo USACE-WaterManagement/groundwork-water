51b3354e2f90974cdfa52f3bc6281b9fc3050f78
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2025-02-24 22:56:00 -0600

    Add code example and finish updating props

--- docs/src/pages/docs/summary/data-status.jsx
+++ docs/src/pages/docs/summary/data-status.jsx
@@ -1,76 +1,137 @@
-
-
-import { Text } from "@usace/groundwork";
+import { Badge, Text } from "@usace/groundwork";
 import ParamsTable from "../../components/params-table";
-import { Code } from "@usace/groundwork";
 import DocsPage from "../_docs-wrapper";
 import Divider from "../../components/divider";
+import { Code } from "../../components/code";
 // import { DataStatus } from "@usace-watermanagement/groundwork-water"
 import DataStatus from "../../../../../lib/components/data/summary/DataStatus";
 
 const returnParams = [
-    {
-        name: "data",
-        type: "object?",
-        desc: (
-            <>
-                Undefined until the request has resolved, then an object containing the
-                response (if a valid response is received). The return object will vary
-                depending on the request endpoint, but response definitions for each CDA
-                endpoint are available in the{" "}
-                <a
-                    href="https://cwms-data.usace.army.mil/cwms-data/swagger-ui.html"
-                    className="underline"
-                >
-                    CDA Swagger Docs
-                </a>
-                .
-            </>
-        ),
-    },
-    {
-        name: "isPending",
-        type: "boolean",
-        desc: "Is true until a query response is received, then false.",
-    },
-    {
-        name: "error",
-        type: "object?",
-        desc: "Is null if no error has occurred, else an object containing the error.",
-    },
+  {
+    name: "tsids",
+    type: "array",
+    desc: "An array of TimeSeries Identifiers to fetch data status for from CDA",
+  },
+  {
+    name: "dataStatusUrl",
+    type: "string",
+    desc: "A DataStatusSummary file URL to fetch additional tsids from. Append to tsids prop. New line delimited. Comments start with ':'",
+  },
+  {
+    name: "office",
+    type: "string",
+    required: true,
+    desc: "The office code for the data status",
+  },
+  {
+    name: "pageSize",
+    type: "number",
+    desc: "The maximum number of entries to fetch in one date range",
+  },
+  {
+    name: "cdaUrl",
+    type: "string",
+    desc: "The URL to the CDA service to fetch TimeSeries from. Defaults to: https://cwms-data.usace.army.mil/cwms-data",
+  },
+  {
+    name: "linkPath",
+    type: "string",
+    desc: "A url path to a project or some other page. I.e. /{office}/projects/ would end up pointing to /{office}/projects/{name}",
+  },
+  {
+    name: "lookBackHours",
+    type: "number",
+    desc: "Number of hours from current time to look back for data status",
+  },
+  {
+    name: "dateFormat",
+    type: "string",
+    desc: (
+      <span>
+        A{" "}
+        <a
+          href="https://day.js.org/docs/en/display/format"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          day.js
+        </a>{" "}
+        format string for the date display in the table
+      </span>
+    ),
+  },
+  {
+    name: "showBadges",
+    type: "boolean",
+    desc: "A flag to determine if the badge color legend should be shown",
+  },
+  {
+    name: "title",
+    type: "string",
+    desc: "The title of the Data Status component within UsaceBox",
+  },
 ];
 
 function DataStatusPage() {
-    return (
-        <DocsPage middleText="Data Status Overview">
-            <div>
-                <Text>
-                    The Data Status component uses quality flags and values from timeseries to
-                    return a quick overview of data in the last number of hours that you specify.
-                    It is not meant to be a replacement of the DataStatusSummary CWMS application,
-                    but certainly can function as a substitute!
-                </Text>
-            </div>
-            <DataStatus office="SWT" tsids={["KEYS.Elev.Inst.1Hour.0.Ccp-Rev"]} dataStatusUrl={"/data/summary/swt.datastatus"} />
-            <Divider text="API Reference" className="mt-8" />
-            <div className="font-bold text-lg pt-6">
-                Return Parameters -{" "}
-                <Code className="p-2">{`const {...} = useQuery()`}</Code>
-            </div>
-            <ParamsTable paramsList={returnParams} showReq={false} />
-            <Text className="pt-3">
-                A full list of the return parameters for useQuery hooks can be
-                referenced in the{" "}
-                <a
-                    href="https://tanstack.com/query/latest/docs/framework/react/reference/useQuery"
-                    className="underline"
-                >
-                    useQuery documentation
-                </a>
-                .
-            </Text>
-        </DocsPage>
-    );
+  return (
+    <DocsPage middleText="Data Status Overview">
+      <div>
+        <Text className="w-3/4 mb-2">
+          The Data Status component uses quality flags and values from
+          timeseries to return a quick overview of data in the last number of
+          hours that you specify. It is not meant to be a replacement of the
+          DataStatusSummary CWMS application, but certainly can function as a
+          substitute!
+        </Text>
+      </div>
+      <DataStatus
+        office="SWT"
+        tsids={["KEYS.Elev.Inst.1Hour.0.Ccp-Rev"]}
+        dataStatusUrl={"/data/summary/swt.datastatus"}
+      />
+      <Divider text="Code Example:" className="mt-8" />
+      <div className="mt-8">
+        <Code className="mt-8" language="jsx">
+          {`import dayjs from "dayjs";
+import { useState } from "react";
+
+/* swt.datastatus Contents : 
+: This is a comment
+KEYS.Elev.Inst.1Hour.0.Ccp-Rev
+ALTU.Precip-Inc.Total.15Minutes.15Minutes.Ccp-Rev
+CLRK.Precip-Inc.Total.15Minutes.15Minutes.Ccp-Rev
+: KEYS.Precip-Inc.Total.1Hour.1Hour.Ccp-Rev
+*/
+
+default export function Example() {
+    <DataStatus
+        office="SWT"
+        tsids={["KEYS.Elev.Inst.1Hour.0.Ccp-Rev"]}
+        dataStatusUrl={"/data/summary/swt.datastatus"}
+        linkPath={"/swt/projects"}
+        lookBackHours={96}
+    />
+}
+`}
+        </Code>
+      </div>
+      <div className="font-bold text-lg pt-6">
+        Data Status Parameters
+        <Code
+          enableCopy={false}
+          className="p-2"
+          language="jsx"
+        >{`<DataStatus ... />`}</Code>
+      </div>
+      <Divider text="API Reference" className="mt-8" />
+      <ParamsTable paramsList={returnParams} showReq={true} />
+      <Badge color="yellow" className="my-2">
+        You must specify either dataStatusUrl or tsids or the table will have no
+        values!
+      </Badge>
+    </DocsPage>
+  );
 }
 
 export { DataStatusPage };
