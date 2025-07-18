f578dd0a57bf2cc339471db7c567f77e6e763e22
Charles Graham SWT <krowven@gmail.com>
2025-07-18 15:19:49 -0500

    Add reverse proxy/development mode

--- vite.config.ts
+++ vite.config.ts
@@ -57,6 +57,34 @@ export default defineConfig(({ mode }) => {
       define: {
         "import.meta.env.PKG_VERSION": JSON.stringify(pkg.version),
       },
+      server: {
+        proxy: {
+          "^/api/v4/.*": {
+            target: "https://midas.sec.usace.army.mil",
+            changeOrigin: true,
+            secure: false,
+            configure: (proxy, _options) => {
+              proxy.on("error", (err, _req, _res) => {
+                console.log("proxy error", err);
+              });
+              proxy.on("proxyReq", (proxyReq, req, _res) => {
+                console.log(
+                  "Sending Request to the Target:",
+                  req.method,
+                  req.url
+                );
+              });
+              proxy.on("proxyRes", (proxyRes, req, _res) => {
+                console.log(
+                  "Received Response from the Target:",
+                  proxyRes.statusCode,
+                  req.url
+                );
+              });
+            },
+          },
+        },
+      },
     };
   }
 });
