2a1663aa713659d90c81df98bec1bf67fa69e2d1
Oskar Hurst <48417811+oskarhurst@users.noreply.github.com>
2025-11-18 16:10:18 -0800

    Set interactive page to fasle

--- docs/src/config/docs.config.js
+++ docs/src/config/docs.config.js
@@ -2,7 +2,7 @@ export const docsConfig = {
   features: {
     // Toggle visibility of the interactive test page
     // Set to true to show in navigation, false to hide
-    showInteractiveTestPage: true,
+    showInteractiveTestPage: false,
 
     // Other feature flags can be added here
   },
