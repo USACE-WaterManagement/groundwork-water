370c0d24b96fbfd248ddfd7d85c8de141ba85da9
Charles Graham <charles.r.graham@usace.army.mil>
2026-04-15 23:35:19 -0500

    Bring in NAE updates

--- docs/src/pages/docs/summary/data-status.jsx
+++ docs/src/pages/docs/summary/data-status.jsx
@@ -8,145 +8,148 @@ import { DataStatus, useDataStatusFile } from "@usace-watermanagement/groundwork
 const BASE_URL = import.meta.env.BASE_URL;
 
 const returnParams = [
-    {
-        name: "office",
-        type: "string",
-        required: true,
-        desc: "The office code for the data status",
-    },
-    {
-        name: "tsids",
-        type: "array",
-        desc: "An array of TimeSeries Identifiers to fetch data status for from CDA",
-    },
-    {
-        name: "pageSize",
-        type: "number",
-        desc: "The maximum number of entries to fetch in one date range",
-    },
-    {
-        name: "cdaUrl",
-        type: "string",
-        desc: "The URL to the CDA service to fetch TimeSeries from. Defaults to: https://cwms-data.usace.army.mil/cwms-data",
-    },
-    {
-        name: "linkPath",
-        type: "string",
-        desc: "A url path to a project or some other page. I.e. /{office}/projects would end up pointing to /{office}/projects/{location}",
-    },
-    {
-        name: "lookBackHours",
-        type: "number",
-        desc: "Number of hours from current time to look back for data status",
-    },
-    {
-        name: "dateFormat",
-        type: "string",
-        desc: (
-            <span>
-                A{" "}
-                <a
-                    href="https://day.js.org/docs/en/display/format"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="underline"
-                >
-                    day.js
-                </a>{" "}
-                format string for the date display in the table
-            </span>
-        ),
-    },
-    {
-        name: "showBadges",
-        type: "boolean",
-        desc: "A flag to determine if the badge color legend should be shown",
-    },
-    {
-        name: "title",
-        type: "string",
-        desc: "The title of the Data Status component within UsaceBox",
-    },
+  {
+    name: "office",
+    type: "string",
+    required: true,
+    desc: "The office code for the data status",
+  },
+  {
+    name: "tsids",
+    type: "array",
+    desc: "An array of TimeSeries Identifiers to fetch data status for from CDA",
+  },
+  {
+    name: "pageSize",
+    type: "number",
+    desc: "The maximum number of entries to fetch in one date range",
+  },
+  {
+    name: "cdaUrl",
+    type: "string",
+    desc: "The URL to the CDA service to fetch TimeSeries from. Defaults to: https://cwms-data.usace.army.mil/cwms-data",
+  },
+  {
+    name: "linkPath",
+    type: "string",
+    desc: "A url path to a project or some other page. I.e. /{office}/projects would end up pointing to /{office}/projects/{location}",
+  },
+  {
+    name: "lookBackHours",
+    type: "number",
+    desc: "Number of hours from current time to look back for data status",
+  },
+  {
+    name: "dateFormat",
+    type: "string",
+    desc: (
+      <span>
+        A{" "}
+        <a
+          href="https://day.js.org/docs/en/display/format"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          day.js
+        </a>{" "}
+        format string for the date display in the table
+      </span>
+    ),
+  },
+  {
+    name: "showBadges",
+    type: "boolean",
+    desc: "A flag to determine if the badge color legend should be shown",
+  },
+  {
+    name: "title",
+    type: "string",
+    desc: "The title of the Data Status component within UsaceBox",
+  },
 ];
 
 const dataStatusFileParams = [
-    {
-        name: "fileUrl",
-        type: "string",
-        required: true,
-        desc: "Path or URL to a newline-delimited data status file.",
-    },
-    {
-        name: "queryOptions",
-        type: "object",
-        desc: (
-            <span>
-                Optional{" "}
-                <a href={`${BASE_URL}#/docs/react-query`} className="underline">
-                    TanStack Query
-                </a>{" "}
-                options passed through to <Code>useQuery</Code>.
-            </span>
-        ),
-    },
+  {
+    name: "fileUrl",
+    type: "string",
+    required: true,
+    desc: "Path or URL to a newline-delimited data status file.",
+  },
+  {
+    name: "queryOptions",
+    type: "object",
+    desc: (
+      <span>
+        Optional{" "}
+        <a href={`${BASE_URL}#/docs/react-query`} className="underline">
+          TanStack Query
+        </a>{" "}
+        options passed through to <Code>useQuery</Code>.
+      </span>
+    ),
+  },
 ];
 
 const dataStatusFileReturnParams = [
-    {
-        name: "data",
-        type: "string[]?",
-        desc: "Resolved TSID entries from the file with comment lines filtered out.",
-    },
-    {
-        name: "isPending",
-        type: "boolean",
-        desc: "True while the file is loading, then false once the query settles.",
-    },
-    {
-        name: "error",
-        type: "object?",
-        desc: "Null when successful, otherwise the fetch/query error.",
-    },
+  {
+    name: "data",
+    type: "string[]?",
+    desc: "Resolved TSID entries from the file with comment lines filtered out.",
+  },
+  {
+    name: "isPending",
+    type: "boolean",
+    desc: "True while the file is loading, then false once the query settles.",
+  },
+  {
+    name: "error",
+    type: "object?",
+    desc: "Null when successful, otherwise the fetch/query error.",
+  },
 ];
 
 function DataStatusPage() {
-    const { data: fileTsids = [], error: fileError, isPending: filePending } = useDataStatusFile({
-        fileUrl: "/data/summary/swt.datastatus",
-    })
-    return (
-        <DocsPage middleText="Data Status Overview">
-            <div>
-                <Text className="w-3/4 mb-2">
-                    The Data Status component uses quality flags and values from
-                    timeseries to return a quick overview of data in the last number of
-                    hours that you specify. It is not meant to be a replacement of the
-                    DataStatusSummary CWMS application, but certainly can function as a
-                    substitute!
-                </Text>
-            </div>
-            {!filePending && fileError ? <div>Failed to load TSID data status file! {fileError?.message}</div> : <DataStatus
-                office="SWT"
-                tsids={["KEYS.Elev.Inst.1Hour.0.Ccp-Rev", ...fileTsids]}
-            />}
-            <Divider text="Code Example:" className="mt-8" />
-            <div className="mt-8">
-                <Code className="mt-8" language="jsx">
-                    {`import dayjs from "dayjs";
+  const {
+    data: fileTsids = [],
+    error: fileError,
+    isPending: filePending,
+  } = useDataStatusFile({
+    fileUrl: "/data/summary/nae.datastatus",
+  });
+  return (
+    <DocsPage middleText="Data Status Overview">
+      <div>
+        <Text className="w-3/4 mb-2">
+          The Data Status component uses quality flags and values from timeseries to
+          return a quick overview of data in the last number of hours that you specify.
+          It is not meant to be a replacement of the DataStatusSummary CWMS application,
+          but certainly can function as a substitute!
+        </Text>
+      </div>
+      {!filePending && fileError ? (
+        <div>Failed to load TSID data status file! {fileError?.message}</div>
+      ) : (
+        <DataStatus office="NAE" tsids={fileTsids} />
+      )}
+      <Divider text="Code Example:" className="mt-8" />
+      <div className="mt-8">
+        <Code className="mt-8" language="jsx">
+          {`import dayjs from "dayjs";
 import { useState } from "react";
 import { useDataStatusFile } from "@usace-watermanagement/groundwork-water";
 
-/* swt.datastatus Contents : 
+/* nae.datastatus Contents : 
 : This is a comment
-KEYS.Elev.Inst.1Hour.0.Ccp-Rev
-ALTU.Precip-Inc.Total.15Minutes.15Minutes.Ccp-Rev
-CLRK.Precip-Inc.Total.15Minutes.15Minutes.Ccp-Rev
-: KEYS.Precip-Inc.Total.1Hour.1Hour.Ccp-Rev
+UVD.Stage-Pool.Inst.15Minutes.0.DCP-rev
+UVD.Stage-TAILWATER.Inst.15Minutes.0.DCP-rev
+: UVD.Precip.Inst.15Minutes.0.DCP-rev
 */
 
 default export function Example() {
     /* This is optional if you want to load tsids from a file */
     const { data: fileTsids, error: fileError, isPending: filePending } = useDataStatusFile({
-        fileUrl: "/data/summary/swt.datastatus",
+        fileUrl: "/data/summary/nae.datastatus",
     })                 
     if (filePending) {
         return <Skeleton className="w-full" />
@@ -156,68 +159,68 @@ default export function Example() {
     }
     /* You could use a timeseries group to load tsids as well! */
     // Or use a static array below :
-    const tsids = ["KEYS.Elev.Inst.1Hour.0.Ccp-Rev"]
+    const tsids = ["UVD.Stage-Pool.Inst.15Minutes.0.DCP-rev"]
     return <DataStatus
-        office="SWT"
+        office="NAE"
         tsids={[...tsids, ...fileTsids]}
     />
 }
 `}
-                </Code>
-            </div>
-            <div className="font-bold text-lg pt-6">
-                Data Status Parameters
-                <Code
-                    enableCopy={false}
-                    className="p-2"
-                    language="jsx"
-                >{`<DataStatus ... />`}</Code>
-            </div>
-            <Divider text="API Reference" className="mt-8" />
-            <ParamsTable paramsList={returnParams} showReq={true} />
-            <Badge color="yellow" className="my-2">
-                You must specify tsids or the table will have no values!
-            </Badge>
-            <Divider text="useDataStatusFile Hook" className="mt-8" />
-            <Text className="mb-2">
-                Use <Code>useDataStatusFile</Code> to load a newline-delimited TSID file
-                and turn it into an array that can be passed into <Code>DataStatus</Code>.
-                Lines beginning with <Code>:</Code> are treated as comments and ignored.
-            </Text>
-            <Code className="mt-4" language="jsx">
-                {`import { useDataStatusFile } from "@usace-watermanagement/groundwork-water";
+        </Code>
+      </div>
+      <div className="font-bold text-lg pt-6">
+        Data Status Parameters
+        <Code
+          enableCopy={false}
+          className="p-2"
+          language="jsx"
+        >{`<DataStatus ... />`}</Code>
+      </div>
+      <Divider text="API Reference" className="mt-8" />
+      <ParamsTable paramsList={returnParams} showReq={true} />
+      <Badge color="yellow" className="my-2">
+        You must specify tsids or the table will have no values!
+      </Badge>
+      <Divider text="useDataStatusFile Hook" className="mt-8" />
+      <Text className="mb-2">
+        Use <Code>useDataStatusFile</Code> to load a newline-delimited TSID file and
+        turn it into an array that can be passed into <Code>DataStatus</Code>. Lines
+        beginning with <Code>:</Code> are treated as comments and ignored.
+      </Text>
+      <Code className="mt-4" language="jsx">
+        {`import { useDataStatusFile } from "@usace-watermanagement/groundwork-water";
 
 function Example() {
     const { data: tsids = [], isPending, error } = useDataStatusFile({
-        fileUrl: "/data/summary/swt.datastatus",
+        fileUrl: "/data/summary/nae.datastatus",
     });
 
     if (isPending) return <div>Loading...</div>;
     if (error) return <div>Failed to load file.</div>;
 
-    return <DataStatus office="SWT" tsids={tsids} />;
+    return <DataStatus office="NAE" tsids={tsids} />;
 }`}
-            </Code>
-            <div className="font-bold text-lg pt-6">
-                Hook Parameters
-                <Code
-                    enableCopy={false}
-                    className="p-2"
-                    language="jsx"
-                >{`useDataStatusFile({...})`}</Code>
-            </div>
-            <ParamsTable paramsList={dataStatusFileParams} showReq={true} />
-            <div className="font-bold text-lg pt-6">
-                Hook Return Parameters
-                <Code
-                    enableCopy={false}
-                    className="p-2"
-                    language="jsx"
-                >{`const { data, isPending, error } = useDataStatusFile({...})`}</Code>
-            </div>
-            <ParamsTable paramsList={dataStatusFileReturnParams} showReq={false} />
-        </DocsPage>
-    );
+      </Code>
+      <div className="font-bold text-lg pt-6">
+        Hook Parameters
+        <Code
+          enableCopy={false}
+          className="p-2"
+          language="jsx"
+        >{`useDataStatusFile({...})`}</Code>
+      </div>
+      <ParamsTable paramsList={dataStatusFileParams} showReq={true} />
+      <div className="font-bold text-lg pt-6">
+        Hook Return Parameters
+        <Code
+          enableCopy={false}
+          className="p-2"
+          language="jsx"
+        >{`const { data, isPending, error } = useDataStatusFile({...})`}</Code>
+      </div>
+      <ParamsTable paramsList={dataStatusFileReturnParams} showReq={false} />
+    </DocsPage>
+  );
 }
 
 export { DataStatusPage };
--- lib/components/data/utilities/qualityDecoder.tsx
+++ lib/components/data/utilities/qualityDecoder.tsx
@@ -1,45 +1,96 @@
 // Author: @stephenkissock
 
 const QUALITY_CODES: { [key: number]: string } = {
-    0: "UNKNOWN",
-    1: "UNKNOWN",
-    3: "OKAY",
-    5: "MISSING",
-    9: "QUESTIONABLE",
-    17: "REJECTED",
-    33: "UNKNOWN",
-    35: "OKAY",
-    37: "MISSING",
-    41: "QUESTIONABLE",
-    49: "REJECTED",
-    65: "UNKNOWN",
-    67: "OKAY",
-    69: "MISSING",
-    73: "QUESTIONABLE",
-    81: "REJECTED",
-    97: "UNKNOWN",
-    99: "OKAY",
-    101: "MISSING",
-    105: "QUESTIONABLE",
-    113: "REJECTED",
-    2433: "UNKNOWN",
-    2435: "OKAY",
-    2437: "MISSING",
-    2441: "QUESTIONABLE",
-    2449: "REJECTED",
-    2465: "UNKNOWN",
-    2467: "OKAY",
-    2469: "MISSING",
-    2473: "QUESTIONABLE",
-    2481: "REJECTED",
-    2497: "UNKNOWN",
-}
+  0: "UNKNOWN",
+  1: "UNKNOWN",
+  3: "OKAY",
+  5: "MISSING",
+  9: "QUESTIONABLE",
+  17: "REJECTED",
+  33: "UNKNOWN",
+  35: "OKAY",
+  37: "MISSING",
+  41: "QUESTIONABLE",
+  49: "REJECTED",
+  65: "UNKNOWN",
+  67: "OKAY",
+  69: "MISSING",
+  73: "QUESTIONABLE",
+  81: "REJECTED",
+  97: "UNKNOWN",
+  99: "OKAY",
+  101: "MISSING",
+  105: "QUESTIONABLE",
+  113: "REJECTED",
+  2433: "UNKNOWN",
+  2435: "OKAY",
+  2437: "MISSING",
+  2441: "QUESTIONABLE",
+  2449: "REJECTED",
+  2465: "UNKNOWN",
+  2467: "OKAY",
+  2469: "MISSING",
+  2473: "QUESTIONABLE",
+  2481: "REJECTED",
+  2497: "UNKNOWN",
+};
+
+const QUALITY_META = {
+  OKAY: {
+    label: "Passed Screening / Validated",
+    shortLabel: "Okay",
+    badgeClassName:
+      "gww-border gww-border-emerald-200 gww-bg-emerald-50 gww-text-emerald-900",
+    segmentClassName: "gww-bg-emerald-300",
+    pillClassName:
+      "gww-bg-emerald-100 gww-text-emerald-900 gww-ring-1 gww-ring-emerald-200",
+  },
+  MISSING: {
+    label: "Missing",
+    shortLabel: "Missing",
+    badgeClassName:
+      "gww-border gww-border-slate-300 gww-bg-slate-100 gww-text-slate-800",
+    segmentClassName: "gww-bg-slate-500",
+    pillClassName: "gww-bg-slate-200 gww-text-slate-900 gww-ring-1 gww-ring-slate-300",
+  },
+  QUESTIONABLE: {
+    label: "Questionable",
+    shortLabel: "Questionable",
+    badgeClassName:
+      "gww-border gww-border-amber-200 gww-bg-amber-50 gww-text-amber-950",
+    segmentClassName: "gww-bg-amber-300",
+    pillClassName: "gww-bg-amber-100 gww-text-amber-950 gww-ring-1 gww-ring-amber-200",
+  },
+  REJECTED: {
+    label: "Rejected",
+    shortLabel: "Rejected",
+    badgeClassName: "gww-border gww-border-rose-200 gww-bg-rose-50 gww-text-rose-900",
+    segmentClassName: "gww-bg-rose-400",
+    pillClassName: "gww-bg-rose-100 gww-text-rose-900 gww-ring-1 gww-ring-rose-200",
+  },
+  UNKNOWN: {
+    label: "Unknown / Undefined",
+    shortLabel: "Unknown",
+    badgeClassName:
+      "gww-border gww-border-fuchsia-200 gww-bg-fuchsia-50 gww-text-fuchsia-900",
+    segmentClassName: "gww-bg-fuchsia-300",
+    pillClassName:
+      "gww-bg-fuchsia-100 gww-text-fuchsia-900 gww-ring-1 gww-ring-fuchsia-200",
+  },
+} as const;
 
 function getQualityStr(ts_value: (number | null)[]) {
-    if (ts_value?.[1] === null)  return QUALITY_CODES[37] 
-    return QUALITY_CODES?.[ts_value?.[2] ?? 0]
+  if (ts_value?.[1] === null) return QUALITY_CODES[37];
+  return QUALITY_CODES?.[ts_value?.[2] ?? 0];
 }
 
-export default getQualityStr
-export { getQualityStr }
-export type { QUALITY_CODES }
+function getQualityMeta(quality?: string | null) {
+  return (
+    QUALITY_META[(quality as keyof typeof QUALITY_META) ?? "UNKNOWN"] ??
+    QUALITY_META.UNKNOWN
+  );
+}
+
+export default getQualityStr;
+export { getQualityMeta, getQualityStr, QUALITY_META };
+export type { QUALITY_CODES };
